d9ccadcf50a516919b1cf10ea344e631407792f6
Collins Igeh <igehac@gmail.com>
2022-04-14 16:05:18 +0100

    completely setup project for hosting at Heroku

--- COLLINSPORTFOLIO/settings.py
+++ COLLINSPORTFOLIO/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-l(8c+jl%i29b)^&z-p*%9$!&-6bh1ikxth*%4b&wlb^@5=1!j-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
@@ -38,12 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     # my apps
     'collins.apps.CollinsConfig',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -139,6 +141,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = ['static/',]
+STATIC_ROOT = 'pro_static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn COLLINSPORTFOLIO.wsgi --log-file -
